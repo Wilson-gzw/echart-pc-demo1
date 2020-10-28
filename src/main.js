6b24210fedfab7c0b16fb504d91073c3d0861f9d
guozhiwei <guozhiwei@sadais.com>
2020-10-28 17:35:56 +0800

    fix: 优化处理

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import Echarts from 'echarts'
+import VueECharts from 'vue-echarts'
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = Echarts
+Vue.component('v-chart', VueECharts)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="home">
-    1234
+    <top-view></top-view>
+    <center-view></center-view>
+    <bottom-view></bottom-view>
+    <map-view></map-view>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TopView from '@/components/top-view/index.vue'
+import BottomView from '@/components/bottom-view/index.vue'
+import CenterView from '@/components/center-view/index.vue'
+import MapView from '@/components/map-view/index.vue'
 
 export default {
   name: 'Home',
   components: {
-
+    TopView, BottomView, CenterView, MapView
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  width:100%;
+  height:100%;
+  padding: 20px;
+  background: #eee;
+  box-sizing: border-box;
+}
+</style>
